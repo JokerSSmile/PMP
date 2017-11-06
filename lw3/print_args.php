93f8de6d3a213ad237932537b90c366f8e57ad7c
Oleg <patrushev1996@gmail.com>
2017-11-06 10:09:59 +0300

    Spaces(((((((

--- lw3/print_args.php
+++ lw3/print_args.php
@@ -4,7 +4,7 @@ $argsCount = $argc - 1;
 if ($argsCount == 0) {
 	echo 'No parameters were specified!';
 } else {
-	echo 'Number of arguments: ' . $argsCount.PHP_EOL;
+	echo 'Number of arguments: ' . $argsCount . PHP_EOL;
 	echo 'Arguments: ';
 	for ($i = 1; $i <= $argsCount; $i++) {
 		echo $argv[$i] . ' ';
