49b49db48ed1749db0e93518c1ff51d087f66698
Oleg <patrushev1996@gmail.com>
2017-11-13 00:49:38 +0300

    lw2 and lw4 fixes

--- lw2/calc_sum.php
+++ lw2/calc_sum.php
@@ -1,55 +1,61 @@
 ﻿<?php
-function strToNum($string)
+
+function getRequestParam($argName)
 {
-	if (!is_numeric($string)) {
-		throw new Exception();
-	}
-
-	return intval($string);
+    if(isset($_GET[$argName]))
+    {
+        return $_GET[$argName];
+    }
+    
+    throw new Exception('Отсутствует требуемый параметр: ' . $argName);
 }
 
-$error = "";
-$out = [];
-parse_str($_SERVER['QUERY_STRING'], $out);
-
-if (count($out) < 3) {
-	$error = "Переданы не все параметры";
-} else if (count($out) > 3) {
-	$error = "Переданы лишние параметры";
-} else if (!isset($_GET["arg1"]) || !isset($_GET["arg2"]) || !isset($_GET["operation"])) {
-	$error = "Один или несколько параметров имеют некорректные имена (например, arg1, arg2 и op)";
+function getRequestNumber($argName)
+{
+	$number = getRequestParam($argName);
+    if(is_numeric($number))
+    {
+        return intval($number);
+	}
+	
+    throw new Exception('Параметры arg1 или arg2 содержат данные, отличные от числовых');
 }
 
-if ($error === "") {
-	try {
-		$arg1 = strToNum($_GET["arg1"]);
-		$arg2 = strToNum($_GET["arg2"]);
-	} catch (Exception $e) {
-		echo "Параметры arg1 и arg2 содержат данные, отличные от числовых";
-		die();
+function calculateResult($operation, $value1, $value2)
+{
+    switch($operation)
+    {
+    case "add":
+        return $value1 + $value2;
+    case "sub":
+        return $value1 - $value2;
+    case "mul":
+        return $value1 * $value2;
+        break;
+    case "div":
+        if($value2 === 0)
+        {
+            throw new Exception('Происходит деление на ноль');
+        }
+        return $value1 / $value2;
+    default:
+        throw new Exception('Параметр operation содержит некорректную операцию');
+    }
+}
+
+try
+{
+    if(count($_GET) > 3)
+    {
+        throw new Exception('Переданы лишние параметры');
+    } else if (count($_GET) < 3) {
+		throw new Exception('Переданы не все параметры, требуются: arg1, arg2 и operation');
 	}
-	$operation = $_GET["operation"];
-	switch ($operation) {
-		case "add":
-			echo $arg1 + $arg2;
-			break;
-		case "sub":
-			echo $arg1 - $arg2;
-			break;
-		case "mul":
-			echo $arg1 * $arg2;
-			break;
-		case "div":
-			if ($arg2 === 0) {
-				echo "Деление на ноль";
-			} else {
-				echo $arg1 / $arg2;
-			}
-			break;
-		default:
-			echo "Параметр operation содержит некорректную операцию";
-			break;
-	}
-} else {
-	echo $error;
+    echo calculateResult(getRequestParam('operation')
+                ,getRequestNumber('arg1')
+                ,getRequestNumber('arg2'));
+}
+catch (Exception $ex)
+{
+    echo $ex->getMessage();
 }
--- lw4/password_strength.php
+++ lw4/password_strength.php
@@ -1,68 +1,111 @@
 <?php
 header('Content-type: application/json');
 
+class PasswordStrengthCalculator
+{
+    private $password;
+    private $passwordLength = 0;
+    private $capitalsCount = 0;
+    private $smallLettersCount = 0;
+    private $digitsCount = 0;
+
+    function __construct($password) {
+        $this->password = $password;
+        $this->passwordLength = strlen($password);
+        $this->capitalsCount = $this->getCapitalsCount($password);
+        $this->smallLettersCount = $this->getSmallLetersCount($password);
+        $this->digitsCount = $this->getDigitsCount($password);
+    }
+
+    private function getDigitsCount($string)
+    {
+        return preg_match_all( "/[0-9]/", $string );
+    }
+
+    private function getCapitalsCount($string)
+    {
+        return preg_match_all( "/[A-Z]/", $string );
+    }
+
+    private function getSmallLetersCount($string)
+    {
+        return preg_match_all( "/[a-z]/", $string );
+    }
+
+    private function isOnlyLetters($string)
+    {
+        return !preg_match('/[A-Za-z]/', $string);
+    }
+
+    private function getDuplicatesCount()
+    {
+        $repeatingCharsCount = 0;
+        $charsCount = array_count_values(str_split($this->password));
+        foreach($charsCount as $count) {
+            if ($count > 1) {
+                $repeatingCharsCount += $count;
+            }
+        }
+    
+        return $repeatingCharsCount;
+    }
+
+    private function getLengthStrength()
+    {
+        return 4 * $this->passwordLength;
+    }
+
+    private function getDigitStrength()
+    {
+        return 4 * $this->digitsCount;
+    }
+
+    private function getCapitalsStrength()
+    {
+        if ($this->capitalsCount > 0) {
+            return 2 * ($this->passwordLength - $this->capitalsCount);
+        }
+        return 0;
+    }
+
+    private function getSmallLettersStrength()
+    {
+        if ($this->smallLettersCount > 0) {
+            return 2 * ($this->passwordLength - $this->smallLettersCount);
+        }
+        return 0;
+    }
+
+    private function getDecreaseBonusForOnlyLetters()
+    {
+        if ($this->isOnlyLetters($this->password)) {
+            return $this->capitalsCount + $this->smallLettersCount;
+        }
+    }
+
+    private function getDecreaseBonusForOnlyDigits()
+    {
+        if ($this->digitsCount === $this->passwordLength) {
+            return $this->digitsCount;
+        }
+    }
+
+    public function calculatePasswordStrength()
+    {
+        return 
+            $this->getLengthStrength() +
+            $this->getDigitStrength() +
+            $this->getCapitalsStrength() +
+            $this->getSmallLettersStrength() -
+            $this->getDecreaseBonusForOnlyLetters() -
+            $this->getDecreaseBonusForOnlyDigits() -
+            $this->getDuplicatesCount();
+    }
+}
+
 if (!isset($_GET['password'])) {
     die();
 }
 
-function getDigitsCount($string)
-{
-    return preg_match_all( "/[0-9]/", $string );
-}
-
-function getCapitalsCount($string)
-{
-    return preg_match_all( "/[A-Z]/", $string );
-}
-
-function getSmallLetersCount($string)
-{
-    return preg_match_all( "/[a-z]/", $string );
-}
-
-function isOnlyLetters($string)
-{
-    return !preg_match('/[A-Za-z]/', $string);
-}
-
-function getDuplicatesCount($string)
-{
-    $repeatingCharsCount = 0;
-    $charsCount = array_count_values(str_split($string));
-    foreach($charsCount as $count) {
-        if ($count > 1) {
-            $repeatingCharsCount += $count;
-        }
-    }
-
-    return $repeatingCharsCount;
-}
-
-function calculatePasswordStrength($password)
-{
-    $strength = 0;
-    $passwordLength = strlen($password);
-    $capitalsCount = getCapitalsCount($password);
-    $smallLettersCount = getSmallLetersCount($password);
-    $digitsCount = getDigitsCount($password);
-
-    $strength += 4 * $passwordLength;
-    $strength += 4 * $digitsCount;
-    if ($capitalsCount > 0) {
-        $strength += 2 * ($passwordLength - $capitalsCount);
-    }
-    if ($smallLettersCount > 0) {
-        $strength += 2 * ($passwordLength - $smallLettersCount);
-    }
-    if (isOnlyLetters($password)) {
-        $strength -= $capitalsCount + $smallLettersCount;
-    }
-    if ($digitsCount === $passwordLength) {
-        $strength -= $digitsCount;
-    }
-    $strength -= getDuplicatesCount($password);
-
-    return $strength;
-}
-
-echo json_encode(array('strength' => calculatePasswordStrength($_GET['password'])));
+$passwordStrengthCalculator = new PasswordStrengthCalculator($_GET['password']);
+echo json_encode(array('strength' => $passwordStrengthCalculator->calculatePasswordStrength()));
